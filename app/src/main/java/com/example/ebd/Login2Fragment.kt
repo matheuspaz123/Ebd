fda2dadc9b41bcbbefdd23772819a7c2f4ef8195
matheuspaz123 <matheuspaz30071998@gmail.com>
2021-04-01 10:36:14 -0300

    Fragment home criado

--- app/src/main/java/com/example/ebd/Login2Fragment.kt
+++ app/src/main/java/com/example/ebd/Login2Fragment.kt
@@ -1,8 +1,8 @@
 package com.example.ebd
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
@@ -24,18 +24,27 @@ class Login2Fragment : Fragment() {
 
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login2, container, false)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //val userr = User("matheus", "12345678")
         binding = FragmentLogin2Binding.bind(view)
         //binding.user = userr
+        binding.loginBt.setOnClickListener {
+             findNavController().navigate(R.id.action_login2Fragment_to_homeFragment)
+        }
 
         
     }
 
-    private fun clickLogar(){
-        //findNavController().navigate()
-    }
+
 
 
 }
