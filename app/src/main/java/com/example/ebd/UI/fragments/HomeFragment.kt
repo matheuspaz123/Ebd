ae9242722924d70cc60fff556547c585500bd07c
matheuspaz123 <matheuspaz30071998@gmail.com>
2021-04-02 03:13:58 -0300

    Fragmento home com grafico

--- app/src/main/java/com/example/ebd/UI/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/ebd/UI/fragments/HomeFragment.kt
@@ -6,9 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.ebd.R
+import com.example.ebd.databinding.FragmentHomeBinding
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+
+val meses = ArrayList<String>()
+val dados = ArrayList<BarEntry>()
 
 
 class HomeFragment : Fragment() {
+
+    lateinit var binding: FragmentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +31,44 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.bind(view)
+
+        dadosGrafico()
+
+        val barDataSet = BarDataSet(dados,"Presentes")
+
+        barDataSet.color = resources.getColor(R.color.grafico)
+
+
+        val datafinal = BarData(barDataSet )
+        binding.homeGrafico.data = datafinal
+        binding.homeGrafico.axisLeft.isEnabled = false
+        binding.homeGrafico.xAxis.isEnabled = false
+        binding.homeGrafico.axisRight.isEnabled = false
+        binding.homeGrafico.isDoubleTapToZoomEnabled = false
+        binding.homeGrafico.animateY(2000)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return view
+    }
+
+    fun dadosGrafico(){
+        //para titulo das colunas
+        meses.add("Janeiro")
+        meses.add("Fevereiro")
+        meses.add("Março")
+        meses.add("Abril")
+
+        //dados das colunas    (coluna , dados)
+        dados.add(BarEntry(0f, 5f))
+        dados.add(BarEntry(1f, 8f))
+        dados.add(BarEntry(2f, 10f))
+        dados.add(BarEntry(3f, 7f))
+
+
+
+
+
     }
 
 
