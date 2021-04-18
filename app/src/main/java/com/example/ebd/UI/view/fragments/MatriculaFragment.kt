3145978dc863892ce3c7786761e60ce739f6bd63
matheuspaz123 <matheuspaz30071998@gmail.com>
2021-04-18 11:04:03 -0300

    layout de matricula

--- app/src/main/java/com/example/ebd/UI/view/fragments/MatriculaFragment.kt
+++ app/src/main/java/com/example/ebd/UI/view/fragments/MatriculaFragment.kt
@@ -6,19 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import android.widget.ArrayAdapter
 import com.example.ebd.R
 import com.example.ebd.data.model.Aluno
 import com.example.ebd.databinding.FragmentLogin2Binding
 import com.example.ebd.databinding.FragmentMatriculaBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_matricula.view.*
 
 
 class MatriculaFragment : Fragment() {
 
     private lateinit var binding: FragmentMatriculaBinding
     private val db = Firebase.firestore
-
+    private var listaClasses = arrayOf(
+        "Jovens",
+        "Crianças",
+        "Adolecentes",
+        "Varões",
+        "Irmãs",
+        "Novos convertidos"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,20 +38,30 @@ class MatriculaFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentMatriculaBinding.bind(
-            inflater.inflate(
-                R.layout.fragment_matricula,
-                container,
-                false
-            )
+
+
+        val view = inflater.inflate(
+            R.layout.fragment_matricula,
+            container,
+            false
         )
+
+        binding = FragmentMatriculaBinding.bind(view)
+
+        var mArrayAdapter = ArrayAdapter(view.context, android.R.layout.simple_spinner_item, listaClasses)
+        //mArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        view.matricula_sp_classe.adapter = mArrayAdapter
+
         var aluno = Aluno("teste", "testeclasse")
 
         binding.matriculaBtSalvar.setOnClickListener {
             aluno = Aluno(
                 binding.matriculaEtNome.text.toString(),
-                binding.matriculaEtClasse.text.toString()
+                "Jovens"
+                //binding.matriculaSpClasse.text.toString()
             )
+
+
 
             db.collection("usuarios")
                 .add(aluno)
@@ -60,7 +79,6 @@ class MatriculaFragment : Fragment() {
                     ).show()
                 }
         }
-
 
 
         // Inflate the layout for this fragment
