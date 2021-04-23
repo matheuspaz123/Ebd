894437cb4d42d9e14bb3325957401e92277f6fad
matheuspaz123 <matheuspaz30071998@gmail.com>
2021-04-23 20:47:26 -0300

    fragment chamada

---
+++ app/src/main/java/com/example/ebd/UI/view/fragments/ChamadaFragment.kt
@@ -0,0 +1,28 @@
+package com.example.ebd.UI.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.ebd.R
+
+
+class ChamadaFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chamada, container, false)
+    }
+
+
+}
