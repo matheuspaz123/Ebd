b37ff2d0b91381e44ce9857cc716712e2b54f17d
matheuspaz123 <matheuspaz30071998@gmail.com>
2021-05-23 23:37:50 -0300

    viewmodel

--- app/src/main/java/com/example/ebd/UI/viewmodel/ChamadaViewModel.kt
+++ app/src/main/java/com/example/ebd/UI/viewmodel/ChamadaViewModel.kt
@@ -3,9 +3,44 @@ package com.example.ebd.UI.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.ebd.data.model.Aluno
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class ChamadaViewModel : ViewModel() {
-    val listaAluno = MutableLiveData<List<Aluno>>()
+    private val listaAluno = MutableLiveData<List<Aluno?>>()
+    private val keys = MutableLiveData<List<String?>>()
+    private val dataBase = FirebaseDatabase.getInstance().reference
+    val classe = MutableLiveData<String>()
+
+
+    init {
+        iniciar()
+    }
+
+    private fun iniciar() {
+        val totalClassesDb = dataBase.child("alunos")
+        totalClassesDb.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val key = ArrayList<String?>()
+                val totalClasses = ArrayList<Aluno?>()
+
+                for (document in snapshot.children) {
+                    key.add(document.key)
+                    totalClasses.add(document.getValue(Aluno::class.java))
+                }
+                listaAluno.postValue(totalClasses)
+                keys.postValue(key)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+
+        })
+    }
+
+    fun getListaAluno() = listaAluno
 
 
 }
